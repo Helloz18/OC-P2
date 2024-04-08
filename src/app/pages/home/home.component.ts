85488e72d771b776eba7e69ec5bbc5231fb26819
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-08 17:19:45 +0200

    display of the chart with ngx-charts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { PieData } from 'src/app/core/models/PieData';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -10,10 +11,33 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
+  pie: PieData[] | undefined = [];
+  title:string =  "test";
+
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe({
+      next: data => {
+        console.log(data);
+        //utiliser map
+        console.log(this.pie)
+        this.pie = data?.map(d => {
+          return {
+            name: d.country,
+            value: d.participations.reduce((acc, nbOfParticiations) => acc + nbOfParticiations.medalsCount, 0)
+          }
+        })
+      }, error: () => {
+
+      }, complete: () => {
+        console.log(this.pie)
+
+      }
+    });
+    
   }
+
 }
