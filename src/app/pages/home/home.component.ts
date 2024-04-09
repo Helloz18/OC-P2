211578e3f75ad5c99450e970fbc75089c234f63f
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-09 18:29:59 +0200

    home component, display of JOs and countries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, toArray } from 'rxjs';
 import { MyPieData } from 'src/app/core/models/MyPieData';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
@@ -15,6 +15,8 @@ export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
   pie: MyPieData[] | undefined = [];
   title:string =  "Medals per country";
+  numberOfJOs: number = 0;
+  numberOfCountries: number = 0;
 
 
   constructor(private olympicService: OlympicService, private router: Router) {}
@@ -29,6 +31,25 @@ export class HomeComponent implements OnInit {
             value: d.participations.reduce((acc, nbOfParticiations) => acc + nbOfParticiations.medalsCount, 0)
           }
         })
+        
+         // calculate number of JOs according to dates
+         let jos: number[] = [];
+         data!.forEach(c => {
+           c.participations.forEach(jo =>
+             jos.push(jo.year))
+         })
+         this.numberOfJOs = Array.from(new Set(jos)).length;
+
+        // calculate number of countries where took place JO
+        let countries: string[] = [];
+        data!.forEach(c => {
+          c.participations.forEach(city =>
+            countries.push(city.city))
+        })
+        this.numberOfCountries = Array.from(new Set(countries)).length;
+        
+       
+        
       }, error: () => {
 
       }, complete: () => {
